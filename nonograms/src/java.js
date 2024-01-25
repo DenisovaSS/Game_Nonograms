3de87fb46a9cebd88faac2a4a8894b38487f3beb
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2024-01-25 17:24:11 +0200

    feat: add grid for matrix

--- nonograms/src/java.js
+++ nonograms/src/java.js
@@ -5,11 +5,27 @@ let screen_width = answer[0].length * cell_size;
 console.log(answer[0].length);
 console.log(answer.length);
 let screen_height = answer.length * cell_size;
+const divContainer = document.createElement("div");
+const managePart = document.createElement("div");
+const gamePart = document.createElement("div");
+const divModals = document.createElement("div");
+const divRect = document.createElement("div");
+
+divModals.className = "modals";
+divContainer.className = "container";
+managePart.className = "manage-part";
+gamePart.className = "game-part";
+document.body.prepend(divContainer);
+document.body.prepend(divModals);
+divContainer.append(gamePart);
+divContainer.prepend(managePart);
+
+//create canvas
 const canvas = document.createElement("canvas");
 canvas.id = "game";
-canvas.width = screen_width + 400;
+canvas.width = screen_width + 500;
 canvas.height = screen_height + 300;
-document.body.prepend(canvas);
+gamePart.append(canvas);
 const ctx = canvas.getContext("2d");
 function buildField() {
   let field = [];
@@ -21,7 +37,7 @@ function buildField() {
     field.push(line);
   }
 
-  return console.log(field);
+  return field;
 }
 buildField();
 //count clues in answer
@@ -44,9 +60,10 @@ function cluesRow(matrix) {
     }
     matrixClueRow.push(countRow);
   }
-  return console.log(matrixClueRow);
+  return matrixClueRow;
 }
-cluesRow(answer);
+let matrixClueRow = cluesRow(answer);
+console.log("matrixClueRow", matrixClueRow);
 function transpose(matrix) {
   const rows = matrix.length,
     cols = matrix[0].length;
@@ -65,7 +82,42 @@ function transpose(matrix) {
 function cluesColumn(matrix) {
   let transMatrix = transpose(matrix);
   // console.log(transMatrix);
-  cluesRow(transMatrix);
+  let matrixCluColumn = cluesRow(transMatrix);
+  return matrixCluColumn;
 }
-
-cluesColumn(answer);
+let matrixClueColumn = cluesColumn(answer);
+console.log("matrixClueColumn", matrixClueColumn);
+//////
+//////let screen_width = answer[0].length * cell_size;
+///// width matrixClueColumn = matrixClueColumn.lenght
+//matrixClueRow
+/////
+//drow this array-answer
+function drawLine(startx, starty, endx, endy, color, line_width) {
+  ctx.strokeStyle = color;
+  ctx.lineWidth = line_width;
+  ctx.beginPath();
+  ctx.moveTo(startx, starty);
+  ctx.lineTo(endx, endy);
+  ctx.stroke();
+}
+// console.log(matrixClueColumn.length);
+ctx.fillStyle = "#cccccc";
+ctx.fillRect(0, 0, screen_width, screen_height);
+function fillFiled() {
+  //row
+  for (let i = 0; i < answer.length + 1; i++) {
+    drawLine(0, i * cell_size, screen_width, i * cell_size, "#443927", 1);
+    if (i % 5 === 0) {
+      drawLine(0, i * cell_size, screen_width, i * cell_size, "#15100A", 3);
+    }
+  }
+  ///column
+  for (let i = 0; i < answer[0].length + 1; i++) {
+    drawLine(i * cell_size, 0, i * cell_size, screen_height, "#443927", 1);
+    if (i % 5 === 0) {
+      drawLine(i * cell_size, 0, i * cell_size, screen_height, "#15100A", 3);
+    }
+  }
+}
+fillFiled();
