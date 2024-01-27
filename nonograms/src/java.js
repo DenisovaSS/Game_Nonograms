c52f8a36835c449e518bde73060ae2f46c8db4bb
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2024-01-27 16:42:19 +0200

    feat: draft version of the drawn field and add all answers

--- nonograms/src/java.js
+++ nonograms/src/java.js
@@ -1,6 +1,7 @@
-const answer = easy.key;
+const answer = normal.crab;
 // console.log(answer);
-let cell_size = 40;
+let cell_size = 20;
+let font_size = cell_size === 20 ? 15 : 30;
 let screen_width = answer[0].length * cell_size;
 console.log(answer[0].length);
 console.log(answer.length);
@@ -23,8 +24,8 @@ divContainer.prepend(managePart);
 //create canvas
 const canvas = document.createElement("canvas");
 canvas.id = "game";
-canvas.width = screen_width + 500;
-canvas.height = screen_height + 300;
+canvas.width = screen_width + 200;
+canvas.height = screen_height + 200;
 gamePart.append(canvas);
 const ctx = canvas.getContext("2d");
 function buildField() {
@@ -83,6 +84,9 @@ function cluesColumn(matrix) {
   let transMatrix = transpose(matrix);
   // console.log(transMatrix);
   let matrixCluColumn = cluesRow(transMatrix);
+  for (let i = 0; i < matrixCluColumn.length; i++) {
+    matrixCluColumn[i].reverse();
+  }
   return matrixCluColumn;
 }
 let matrixClueColumn = cluesColumn(answer);
@@ -102,22 +106,84 @@ function drawLine(startx, starty, endx, endy, color, line_width) {
   ctx.stroke();
 }
 // console.log(matrixClueColumn.length);
-ctx.fillStyle = "#cccccc";
-ctx.fillRect(0, 0, screen_width, screen_height);
+ctx.fillStyle = "#ffffff";
+ctx.fillRect(cell_size * 5, cell_size * 5, screen_width, screen_height);
 function fillFiled() {
   //row
-  for (let i = 0; i < answer.length + 1; i++) {
-    drawLine(0, i * cell_size, screen_width, i * cell_size, "#443927", 1);
+  for (let i = 0; i < answer.length + 6; i++) {
+    drawLine(
+      0,
+      i * cell_size,
+      screen_width + cell_size * 5,
+      i * cell_size,
+      "#443927",
+      1,
+    );
     if (i % 5 === 0) {
-      drawLine(0, i * cell_size, screen_width, i * cell_size, "#15100A", 3);
+      drawLine(
+        0,
+        i * cell_size,
+        screen_width + cell_size * 5,
+        i * cell_size,
+        "#15100A",
+        3,
+      );
     }
   }
   ///column
-  for (let i = 0; i < answer[0].length + 1; i++) {
-    drawLine(i * cell_size, 0, i * cell_size, screen_height, "#443927", 1);
+  for (let i = 0; i < answer[0].length + 6; i++) {
+    drawLine(
+      i * cell_size,
+      0,
+      i * cell_size,
+      screen_height + cell_size * 5,
+      "#443927",
+      1,
+    );
     if (i % 5 === 0) {
-      drawLine(i * cell_size, 0, i * cell_size, screen_height, "#15100A", 3);
+      drawLine(
+        i * cell_size,
+        0,
+        i * cell_size,
+        screen_height + cell_size * 5,
+        "#15100A",
+        3,
+      );
     }
   }
 }
+
 fillFiled();
+ctx.font = `${font_size}px helvetica`;
+// ctx.textBaseline = "middle";
+ctx.textBaseline = "ideographic";
+ctx.textAlign = "right";
+ctx.fillStyle = "#4400ff";
+// Line hints
+for (let i = 0; i < matrixClueRow.length; i++) {
+  let hint = " ";
+  for (let j = 0; j < matrixClueRow[i].length; j++) {
+    hint += matrixClueRow[i][j] + "  ";
+  }
+  if (hint == " ") {
+    hint = "| 0 |";
+  }
+  ctx.fillText(hint, cell_size * 5, cell_size * (i + 1) + cell_size * 5);
+}
+ctx.fillStyle = "#BA5809";
+ctx.textAlign = "start";
+// Column
+
+for (let i = 0; i < matrixClueColumn.length; i++) {
+  let part = " ";
+  for (let j = matrixClueColumn[i].length - 1; j >= 0; j--) {
+    // console.log(i, j);
+    // console.log(matrixClueColumn[i][j]);
+    part = matrixClueColumn[i][j];
+    if (part == " ") {
+      part = 0;
+    }
+    ctx.fillText(part, cell_size * 5 + cell_size * i + 10, cell_size * (5 - j));
+    //ctx.fillText(part, cell_size * 5 + cell_size * i, cell_size * (j + 3));
+  }
+}
